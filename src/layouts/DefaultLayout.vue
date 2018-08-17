1fb72fd25bd6ede6ac74ea3e9790f9b937b90043
Javier Diaz <coderdiaz@gmail.com>
2018-08-17 13:47:04 -0500

    added layout

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="default-layout">
+    <!-- header -->
+    <header-partial></header-partial>
+    <!-- end header -->
+    <!-- filter section -->
+    <slot name="filters"></slot>
+    <!-- end filter section -->
+    <!-- main -->
+    <main class="main-section mb-5">
+      <div class="container">
+        <slot name="content"></slot>
+      </div>
+    </main>
+    <!-- end main -->
+    <!-- footer -->
+    <footer-partial></footer-partial>
+    <!-- end footer -->
+  </div>
+</template>
+
+<script>
+import HeaderPartial from '@/partials/HeaderPartial'
+import FooterPartial from '@/partials/FooterPartial'
+export default {
+  name: 'DefaultLayout',
+  components: {
+    HeaderPartial,
+    FooterPartial
+  }
+}
+</script>
