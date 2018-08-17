e8b30e587258366a454a5067f34064ac3abf5d64
Javier Diaz <coderdiaz@gmail.com>
2018-08-17 13:46:57 -0500

    added partials

---
+++ src/partials/FooterPartial.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <ul class="nav justify-content-center">
+        <li class="nav-item">
+          <span class="nav-link">Vue Pokedex</span>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterPartial'
+}
+</script>
---
+++ src/partials/HeaderPartial.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <nav class="text-center navbar-dark">
+        <router-link class="navbar-brand" :to="{ name: 'Home' }">
+          <img src="../assets/logo_pokemon.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+          Pokédex Virtual
+        </router-link>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderPartial'
+}
+</script>
