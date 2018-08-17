955e90a0718bbca2128bdc26495b8e46e54afc65
Javier Diaz <coderdiaz@gmail.com>
2018-08-17 13:48:16 -0500

    added logic vuex

--- src/store.js
+++ src/store.js
@@ -1,10 +1,40 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    pokemon: [],
+    limit: 12,
+    filter: ''
+  },
+  actions: {
+    LOAD_POKEMON_LIST: ({ commit }) => {
+      axios.get(`${process.env.VUE_APP_API_URL}/pokemon.json`)
+        .then(response => commit('SET_POKEMON_LIST', { data: response.data }))
+        .catch(err => console.error(err))
+    },
+    QUERY: ({ commit }, payload) => {
+      commit('SET_FILTER_QUERY', { query: payload })
+    }
+  },
+  mutations: {
+    SET_POKEMON_LIST: (state, { data }) => {
+      state.pokemon = data
+    },
+    SET_FILTER_QUERY: (state, { query }) => {
+      state.filter = query
+    }
+  },
+  getters: {
+    filteredPokemon: state => {
+      const filteredPokemon = (state.filter === '') ? state.pokemon : state.pokemon.filter(item => {
+        const itemName = item.name.toLowerCase()
+        return itemName.includes(state.filter.toLowerCase())
+      })
+      return filteredPokemon.slice(0, state.limit)
+    }
+  }
 })
