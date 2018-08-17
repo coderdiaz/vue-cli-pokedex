3523474070d41d821ceb012c12b628f3c49933c0
Javier Diaz <coderdiaz@gmail.com>
2018-08-17 13:48:25 -0500

    added routes for webapp

--- src/router.js
+++ src/router.js
@@ -1,10 +1,25 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+// Views
+import HomeView from '@/views/Home'
+import DetailPokemonView from '@/views/DetailPokemon'
+
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: []
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: HomeView
+    },
+    {
+      path: 'detail/:pokemonName',
+      name: 'DetailPokemon',
+      component: DetailPokemonView
+    }
+  ]
 })
