d52fe2ec688c94fd40f40a275f0adbf5600c373c
Javier Diaz <coderdiaz@gmail.com>
2018-08-17 13:47:41 -0500

    added pokedex components

---
+++ src/components/PokedexCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="pokedex-card">
+    <img class="pokedex-card-img" :src="pokemon.ThumbnailImage"/>
+    <div class="pokedex-card-details">
+      <small class="pokedex-card-details__number">#{{pokemon.number}}</small>
+      <h4 class="pokedex-card-details__title">{{pokemon.name}}</h4>
+      <div class="pokedex-card__types">
+        <pokedex-tag
+          v-for="(type, index) in pokemon.type"
+          :key="index"
+          :pokemon-type="type"></pokedex-tag>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import PokedexTag from '@/components/PokedexTag'
+export default {
+  name: 'PokedexCard',
+  props: {
+    pokemon: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    PokedexTag
+  }
+}
+</script>
---
+++ src/components/PokedexTag.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  name: 'PokedexTag',
+  props: {
+    pokemonType: {
+      type: String,
+      required: true
+    }
+  },
+  render (h) {
+    return <span class={this.getClassTag()}>
+      {this.pokemonType}
+    </span>
+  },
+  methods: {
+    getClassTag () {
+      return {
+        [`pokedex-tag-${this.pokemonType}`]: true,
+        'pokedex-tag': true,
+        'badge': true,
+        'mr-2': true
+      }
+    }
+  }
+}
+</script>
