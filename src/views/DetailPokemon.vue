c473d11fdc733907b1e736d39f272992895365e1
Javier Diaz <hello@coderdiaz.me>
2018-11-23 13:02:30 -0600

    fix: updated detail-pokemon vue

--- src/views/DetailPokemon.vue
+++ src/views/DetailPokemon.vue
@@ -5,54 +5,42 @@ import PokedexTag from '@/components/PokedexTag'
 export default {
   name: 'DetailPokemonView',
   render () {
-    let pokemon = this.$route.params.pokemon
+    const pokemon = this.$route.params.pokemon
     if (!pokemon) {
-      this.$router.push({ name: 'Home' })
-    } else {
-      // Creating tags for types
-      const pokemonTags = () => {
-        return pokemon.type.map((type) => {
-          return <PokedexTag pokemonType={type}></PokedexTag>
-        })
-      }
+      return this.$router.push({name: 'Home'})
+    }
 
-      const weaknessTags = () => {
-        return pokemon.weakness.map((type) => {
-          return <PokedexTag pokemonType={type}></PokedexTag>
-        })
-      }
+    // Creating tags for types and weekness
+    const pokemonTags = _ => pokemon.type.map(type => <PokedexTag pokemonType={type}></PokedexTag>)
+    const weaknessTags = _ => pokemon.weakness.map((type) => <PokedexTag pokemonType={type}></PokedexTag>)
 
-      return <DefaultLayout>
-        <section slot="content" class="pokedex-item mt-5">
-          <div class="row">
-            <div class="col-md-12">
-              <h1 class="pokedex-detail__title text-center mb-5">
-                {pokemon.name}
-                <span class="text-muted">#{pokemon.number}</span>
-              </h1>
-              <hr/>
-              <div class="row">
-                <div class="col-md-5">
-                  <img class="pokedex-detail__image" src={this.getUrlThumbnail}/>
-                </div>
-                <div class="col-md-7">
-                  <h4>Tipo</h4>
-                  {pokemonTags()}
-                  <h4 class="mt-3">Debilidades</h4>
-                  {weaknessTags()}
-                </div>
+    return <DefaultLayout>
+      <section slot="content" class="pokedex-item mt-5">
+        <div class="row">
+          <div class="col-md-12">
+            <h1 class="pokedex-detail__title text-center mb-5">
+              {pokemon.name}
+              <span class="text-muted">#{pokemon.number}</span>
+            </h1>
+            <hr/>
+            <div class="row">
+              <div class="col-md-5">
+                <img class="pokedex-detail__image" src={this.getUrlThumbnail(pokemon)}/>
+              </div>
+              <div class="col-md-7">
+                <h4>Tipo</h4>
+                {pokemonTags()}
+                <h4 class="mt-3">Debilidades</h4>
+                {weaknessTags()}
               </div>
             </div>
           </div>
-        </section>
-      </DefaultLayout>
-    }
+        </div>
+      </section>
+    </DefaultLayout>
   },
-  computed: {
-    getUrlThumbnail () {
-      let pokemon = this.$route.params.pokemon
-      return (!pokemon) ? '' : `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemon.number}.png`
-    }
+  methods: {
+    getUrlThumbnail: (pokemon) => `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${pokemon.number}.png`
   }
 }
 </script>
